f62fa2a4d9f969e88391d99bbd5bae59a51a744f
Benrise <benrise_x@mail.ru>
2025-01-31 06:59:06 +0000

    Production deploy

--- bot/docker-entrypoint.sh
+++ bot/docker-entrypoint.sh
@@ -2,5 +2,5 @@
 
 PORT=${API_PORT:-8000}
 
-exec uvicorn main:app --host 0.0.0.0 --port ${PORT} --reload
-# exec gunicorn -k uvicorn.workers.UvicornWorker -w 10 -b 0.0.0.0:${PORT} main:app
+# exec uvicorn main:app --host 0.0.0.0 --port ${PORT} --reload
+exec gunicorn -k uvicorn.workers.UvicornWorker -w 4 -b 0.0.0.0:${PORT} main:app
